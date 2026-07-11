f74c28c608292303f0b54cf412cf18bd6255860e
Enrique Roncal <systems.roncal@gmail.com>
2026-07-10 19:49:15 -0500

    Fix Netlify TS error: declare react-syntax-highlighter styles module

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -3,3 +3,8 @@ declare module '*.jpeg';
 declare module '*.png';
 declare module '*.svg';
 declare module '*.webp';
+
+declare module 'react-syntax-highlighter/dist/esm/styles/hljs';
+declare module 'react-syntax-highlighter/dist/cjs/styles/hljs';
+declare module 'react-syntax-highlighter/dist/esm/styles/prism';
+declare module 'react-syntax-highlighter';
